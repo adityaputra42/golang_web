11c2639317ea653cd26ff4ec404e94a0e59dea80
adityaputra42 <aditiyaputra42@gmail.com>
2023-08-31 15:29:51 +0700

    add query parameter

---
+++ query_param_test.go
@@ -0,0 +1,70 @@
+package golangweb
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func SayHello(writer http.ResponseWriter, request *http.Request) {
+	name := request.URL.Query().Get("name")
+	if name == "" {
+		fmt.Fprint(writer, "Hello")
+	} else {
+		fmt.Fprintf(writer, "Hello %s", name)
+	}
+}
+
+func TestQueryParameter(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://locahost:8080/hello?", nil)
+	recorder := httptest.NewRecorder()
+
+	SayHello(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	bodyString := string(body)
+
+	fmt.Println(bodyString)
+}
+
+func MultipleQueryParameter(writer http.ResponseWriter, request *http.Request) {
+	firstName := request.URL.Query().Get("first_name")
+	lastName := request.URL.Query().Get("last_name")
+	fmt.Fprintf(writer, "Hello %s %s", firstName, lastName)
+}
+
+func TestMultipleQueryParameter(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://locahost:8080/hello?first_name=Aditya&last_name=Pratama", nil)
+	recorder := httptest.NewRecorder()
+
+	MultipleQueryParameter(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	bodyString := string(body)
+
+	fmt.Println(bodyString)
+}
+
+func MultipleParameterValues(writer http.ResponseWriter, request *http.Request) {
+	query := request.URL.Query()
+	names := query["name"]
+
+	fmt.Fprintf(writer, "Hello %s ", strings.Join(names, " "))
+}
+func TestMultipleParameterValue(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://locahost:8080/hello?name=adit&name=main", nil)
+	recorder := httptest.NewRecorder()
+
+	MultipleParameterValues(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	bodyString := string(body)
+
+	fmt.Println(bodyString)
+}

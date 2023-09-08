3df880f25362e68a8acccc627d658895376d82d7
adityaputra42 <aditiyaputra42@gmail.com>
2023-09-08 22:39:06 +0700

    add contoh cross site scripting(XSS)

--- auto_escape_test.go
+++ auto_escape_test.go
@@ -2,6 +2,7 @@ package golangweb
 
 import (
 	"fmt"
+	"html/template"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -27,6 +28,52 @@ func TestAutoEscapeServer(t *testing.T) {
 	server := http.Server{
 		Addr:    "localhost:8080",
 		Handler: http.HandlerFunc(AutoEscape),
+	}
+	err := server.ListenAndServe()
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func AutoEscapeDisabled(writer http.ResponseWriter, request *http.Request) {
+	myTemplate.ExecuteTemplate(writer, "post.gohtml", map[string]interface{}{
+		"Title": "Template Auto Escape",
+		"Body":  template.HTML("<h2>Iki lho body</h2>"),
+	})
+}
+
+func TestAutoEscapeDisabledServer(t *testing.T) {
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: http.HandlerFunc(AutoEscapeDisabled),
+	}
+	err := server.ListenAndServe()
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TemplateXSS(writer http.ResponseWriter, request *http.Request) {
+	myTemplate.ExecuteTemplate(writer, "post.gohtml", map[string]interface{}{
+		"Title": "Template Auto Escape",
+		"Body":  template.HTML(request.URL.Query().Get("body")),
+	})
+}
+
+func TestTemplateXSS(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/?body=<p>xss</p>", nil)
+	recorder := httptest.NewRecorder()
+	TemplateXSS(recorder, request)
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
+
+func TestTemplateXSSServer(t *testing.T) {
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: http.HandlerFunc(TemplateXSS),
 	}
 	err := server.ListenAndServe()
 

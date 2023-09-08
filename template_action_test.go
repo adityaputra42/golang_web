9ae52cea21dea00cbb3c3e90e65c05bf34924630
adityaputra42 <aditiyaputra42@gmail.com>
2023-09-08 13:23:07 +0700

    add template action test

--- template_action_test.go
+++ template_action_test.go
@@ -31,3 +31,69 @@ func TestTempleateActionMap(t *testing.T) {
 	body, _ := io.ReadAll(recorder.Result().Body)
 	fmt.Println(string(body))
 }
+
+func TemplateActionOperator(writer http.ResponseWriter, request *http.Request) {
+
+	t := template.Must(template.ParseFiles("./templates/operator.gohtml"))
+
+	t.ExecuteTemplate(writer, "operator.gohtml", map[string]interface{}{
+		"Title":      "Template data map",
+		"FinalValue": 70,
+	})
+}
+
+func TestTempleateActionOperator(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateActionOperator(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
+
+func TemplateActionRange(writer http.ResponseWriter, request *http.Request) {
+
+	t := template.Must(template.ParseFiles("./templates/range.gohtml"))
+
+	t.ExecuteTemplate(writer, "range.gohtml", map[string]interface{}{
+		"Title": "Template data Range",
+		"Hobbies": []string{
+			"Game", "Tidur", "Masak", "Makan", "Ngoding",
+		},
+	})
+}
+
+func TestTempleateActionRange(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateActionRange(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
+
+func TemplateActionWith(writer http.ResponseWriter, request *http.Request) {
+
+	t := template.Must(template.ParseFiles("./templates/with.gohtml"))
+
+	t.ExecuteTemplate(writer, "with.gohtml", map[string]interface{}{
+		"Title": "Template data Range",
+		"Name":  "Aditya Putra",
+		// "Address": map[string]interface{}{
+		// 	"Street": "Jalan Sempor Baru",
+		// 	"City":   "Kebumen",
+		// },
+	})
+}
+
+func TestTempleateActionWith(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateActionWith(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
